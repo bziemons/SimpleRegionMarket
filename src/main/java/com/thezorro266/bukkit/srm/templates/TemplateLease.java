09f3c65c8f2cab620c56c047cf1806fb7bc7b383
theZorro266 <root@theZorro266.com>
2014-01-03 03:51:06 +0100

    Prepared lease template: added TODO placeholders

--- src/main/java/com/thezorro266/bukkit/srm/templates/TemplateLease.java
+++ src/main/java/com/thezorro266/bukkit/srm/templates/TemplateLease.java
@@ -1,26 +1,35 @@
 /**
  * SimpleRegionMarket
  * Copyright (C) 2013-2014  theZorro266 <http://www.thezorro266.com>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package com.thezorro266.bukkit.srm.templates;
 
+import java.util.HashMap;
+import org.bukkit.ChatColor;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.block.Block;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
+import com.thezorro266.bukkit.srm.SimpleRegionMarket;
+import com.thezorro266.bukkit.srm.exceptions.ContentSaveException;
 import com.thezorro266.bukkit.srm.factories.RegionFactory;
+import com.thezorro266.bukkit.srm.factories.SignFactory;
+import com.thezorro266.bukkit.srm.helpers.Location;
 import com.thezorro266.bukkit.srm.templates.interfaces.TimedTemplate;
 
 public class TemplateLease extends TemplateSell implements TimedTemplate {
@@ -31,7 +40,13 @@ public class TemplateLease extends TemplateSell implements TimedTemplate {
 
 	@Override
 	public void schedule() {
+		for (RegionFactory.Region region : regionList) {
+			if (isRegionOccupied(region)) {
+				int currentSecs = (int) (System.nanoTime() / 1000000000);
 
+				// TODO: Check for expiration, try to expand the time, send the owner a message, and clearRegion, if expanding is not possible
+			}
+		}
 	}
 
 	@Override
@@ -39,169 +54,140 @@ public class TemplateLease extends TemplateSell implements TimedTemplate {
 		return false;
 	}
 
-	/*
 	@Override
-	public void takeRegion(Player newOwner, String world, String region) {
-		final ProtectedRegion protectedRegion = AlphaRegionMarket.instance.wgManager.getProtectedRegion(Bukkit.getWorld(world), region);
-
-		if (Utils.getEntryBoolean(this, world, region, "taken")) {
-			final Player oldOwner = Bukkit.getPlayer(Utils.getEntryString(this, world, region, "owner"));
-			final ArrayList<String> list = new ArrayList<String>();
-			list.add(region);
-			list.add(newOwner.getName());
-			langHandler.playerNormalOut(oldOwner, "PLAYER.REGION.JUST_TAKEN_BY", list);
-			releaseRegion(world, region);
-		} else {
-			// Clear Members and Owners
-			protectedRegion.setMembers(new DefaultDomain());
-			protectedRegion.setOwners(new DefaultDomain());
+	public boolean setRegionOccupied(RegionFactory.Region region, boolean isOccupied) {
+		if (!isOccupied) {
+			// TODO: Unset renttime
+			region.setOption("owner", null);
 		}
-
-		protectedRegion.getMembers().addPlayer(AlphaRegionMarket.instance.wgManager.wrapPlayer(newOwner));
-
-		Utils.setEntry(this, world, region, "taken", true);
-		Utils.setEntry(this, world, region, "owner", newOwner.getName());
-		Utils.setEntry(this, world, region, "expiredate", System.currentTimeMillis() + Utils.getEntryLong(this, world, region, "renttime"));
-
-		final ArrayList<String> list = new ArrayList<String>();
-		list.add(region);
-		langHandler.playerNormalOut(newOwner, "PLAYER.REGION.RENT", list);
-
-		tokenManager.updateSigns(this, world, region);
-	}
-
-	@Override
-	public void releaseRegion(String world, String region) {
-		final ProtectedRegion protectedRegion = AlphaRegionMarket.instance.wgManager.getProtectedRegion(Bukkit.getWorld(world), region);
-
-		// Clear Members and Owners
-		protectedRegion.setMembers(new DefaultDomain());
-		protectedRegion.setOwners(new DefaultDomain());
-
-		Utils.setEntry(this, world, region, "taken", false);
-		Utils.removeEntry(this, world, region, "owner");
-		Utils.removeEntry(this, world, region, "expiredate");
-
-		tokenManager.updateSigns(this, world, region);
-	}
-
-	@Override
-	public boolean signCreated(Player player, String world, ProtectedRegion protectedRegion, Location signLocation, HashMap<String, String> input,
-			String[] lines) {
-		final String region = protectedRegion.getId();
-
-		if (!entries.containsKey(world) || !entries.get(world).containsKey(region)) {
-			final double priceMin = Utils.getOptionDouble(this, "price.min");
-			final double priceMax = Utils.getOptionDouble(this, "price.max");
-			double price;
-			if (AlphaRegionMarket.instance.econManager.isEconomy()) {
-				if (input.get("price") != null) {
-					try {
-						price = Double.parseDouble(input.get("price"));
-					} catch (final Exception e) {
-						langHandler.playerErrorOut(player, "PLAYER.ERROR.NO_PRICE", null);
-						return false;
-					}
-				} else {
-					price = priceMin;
-				}
-			} else {
-				price = 0;
-			}
-
-			if (priceMin > price && (priceMax == -1 || price < priceMax)) {
-				final ArrayList<String> list = new ArrayList<String>();
-				list.add(String.valueOf(priceMin));
-				list.add(String.valueOf(priceMax));
-				langHandler.playerErrorOut(player, "PLAYER.ERROR.PRICE_LIMIT", list);
-				return false;
-			}
-
-			final long renttimeMin = Utils.getOptionLong(this, "renttime.min");
-			final long renttimeMax = Utils.getOptionLong(this, "renttime.max");
-			long renttime;
-			if (!input.get("time").isEmpty()) {
-				try {
-					renttime = Utils.parseSignTime(input.get("time"));
-				} catch (final Exception e) {
-					langHandler.playerErrorOut(player, "PLAYER.ERROR.NO_RENTTIME", null);
-					return false;
-				}
-			} else {
-				langHandler.playerErrorOut(player, "PLAYER.ERROR.NO_RENTTIME", null);
-				return false;
-			}
-
-			if (renttimeMin > renttime && (renttimeMax == -1 || renttime < renttimeMax)) {
-				final ArrayList<String> list = new ArrayList<String>();
-				list.add(String.valueOf(renttimeMin));
-				list.add(String.valueOf(renttimeMax));
-				langHandler.playerErrorOut(player, "PLAYER.ERROR.RENTTIME_LIMIT", null);
-				return false;
-			}
-
-			Utils.setEntry(this, world, region, "price", price);
-			Utils.setEntry(this, world, region, "renttime", renttime);
-			Utils.setEntry(this, world, region, "account", player.getName());
-			Utils.setEntry(this, world, region, "taken", false);
-			Utils.removeEntry(this, world, region, "owner");
-		}
-
-		final ArrayList<Location> signLocations = Utils.getSignLocations(this, world, region);
-		signLocations.add(signLocation);
-		if (signLocations.size() == 1) {
-			Utils.setEntry(this, world, region, "signs", signLocations);
-		}
-
-		tokenManager.updateSigns(this, world, region);
+		region.setOption("state", (isOccupied ? "occupied" : "free"));
 		return true;
 	}
 
 	@Override
-	public Map<String, String> getReplacementMap(String world, String region) {
-		final HashMap<String, String> replacementMap = (HashMap<String, String>) super.getReplacementMap(world, region);
-		if (replacementMap != null) {
-			replacementMap.put("time", Utils.getSignTime(Utils.getEntryLong(this, world, region, "renttime")));
-			if (Utils.getEntry(this, world, region, "expiredate") != null) {
-				replacementMap.put("timeleft", Utils.getSignTime(Utils.getEntryLong(this, world, region, "expiredate") - System.currentTimeMillis()));
+	public void clickSign(Player player, SignFactory.Sign sign) {
+		RegionFactory.Region region = sign.getRegion();
+		if (isRegionOccupied(region)) {
+			if (isRegionOwner(player, region)) {
+				player.sendMessage("This is your region.");
+			} else {
+				player.sendMessage("This region is already sold.");
 			}
+		} else {
+			// TODO: Player permissions
+			// TODO: Player money
+			clearRegion(region);
+			if (buyerIsOwner) {
+				setRegionOwners(region, new OfflinePlayer[] { player });
+			} else {
+				setRegionMembers(region, new OfflinePlayer[] { player });
+			}
+
+			// TODO: Set renttime
+			region.setOption("owner", player.getName());
+			setRegionOccupied(region, true);
+
+			try {
+				SimpleRegionMarket.getInstance().getTemplateManager().saveRegion(region);
+			} catch (ContentSaveException e) {
+				player.sendMessage(ChatColor.RED + "Could not save region");
+				SimpleRegionMarket.getInstance().getLogger().severe("Could not save region " + region.getName());
+				SimpleRegionMarket.getInstance().printError(e);
+			}
+
+			player.sendMessage("You're now the owner of this region");
 		}
-		return replacementMap;
+		region.updateSigns();
 	}
 
 	@Override
-	public boolean canAddOwner() {
-		return false;
+	public void replacementMap(RegionFactory.Region region, HashMap<String, String> replacementMap) {
+		super.replacementMap(region, replacementMap);
+
+		// TODO: Add renttime, owner, etc. to the replacement map
 	}
 
 	@Override
-	public void schedule(String world, String region) {
-		if (Utils.getEntryBoolean(this, world, region, "taken")) {
-			if (Utils.getEntryLong(this, world, region, "expiredate") < System.currentTimeMillis()) {
-				if (Utils.getEntry(this, world, region, "owner") != null) {
-					final String owner = Utils.getEntryString(this, world, region, "owner");
-					final String account = Utils.getEntryString(this, world, region, "account");
-					final Double price = Utils.getEntryDouble(this, world, region, "price");
-					final Player player = Bukkit.getPlayer(owner);
-					if (AlphaRegionMarket.instance.econManager.econHasEnough(owner, price)) {
-						if (AlphaRegionMarket.instance.econManager.moneyTransaction(owner, account, price)) {
-							if (player != null) {
-								final ArrayList<String> list = new ArrayList<String>();
-								list.add(region);
-								langHandler.playerNormalOut(player, "PLAYER.REGION.AUTO_EXPANDED", list);
+	public SignFactory.Sign makeSign(Player player, Block block, HashMap<String, String> inputMap) {
+		ProtectedRegion worldguardRegion = RegionFactory.getProtectedRegionFromLocation(Location.fromBlock(block), inputMap.remove("region"));
+
+		if (worldguardRegion != null) {
+			RegionFactory.Region region = null;
+			for (RegionFactory.Region regionEntry : regionList) {
+				if (regionEntry.getWorldguardRegion().equals(worldguardRegion)) {
+					region = regionEntry;
+					break;
+				}
+			}
+
+			if (region == null) {
+				region = RegionFactory.instance.createRegion(this, block.getWorld(), worldguardRegion);
+
+				double price;
+				if (SimpleRegionMarket.getInstance().getVaultHook().getEconomy() != null) {
+					String priceString = inputMap.remove("price");
+					if (priceString != null) {
+						try {
+							price = Double.parseDouble(priceString);
+						} catch (final Exception e) {
+							player.sendMessage("Price not found.");
+							return null;
+						}
+					} else {
+						price = priceMin;
+					}
+				} else {
+					price = 0;
+				}
+
+				if (priceMin > price && (priceMax == -1 || price < priceMax)) {
+					String priceMinString;
+					String priceMaxString;
+					try {
+						priceMinString = SimpleRegionMarket.getInstance().getVaultHook().getEconomy().format(priceMin);
+						priceMaxString = SimpleRegionMarket.getInstance().getVaultHook().getEconomy().format(priceMax);
+					} catch (Throwable e) {
+						priceMinString = String.format("%.2f", priceMin);
+						priceMaxString = String.format("%.2f", priceMax);
+					}
+					player.sendMessage(String.format(ChatColor.RED + "The price must be between %s and %s", priceMinString, priceMaxString));
+					return null;
+				}
+
+				String account = player.getName();
+				{
+					String accountString = inputMap.remove("account");
+					if (accountString != null) {
+						if (SimpleRegionMarket.getInstance().getVaultHook().hasPermission(player, String.format("simpleregionmarket.%s.setaccount", getId()))) {
+							if (accountString.equalsIgnoreCase("none")) {
+								account = "";
+							} else {
+								account = accountString;
 							}
-							return;
 						}
 					}
-					if (player != null) {
-						final ArrayList<String> list = new ArrayList<String>();
-						list.add(region);
-						langHandler.playerNormalOut(player, "PLAYER.REGION.EXPIRED", list);
-					}
 				}
-				releaseRegion(world, region);
+
+				// TODO: Check for renttime
+
+				region.setOption("price", price);
+				region.setOption("account", account);
+				clearRegion(region);
 			}
+
+			SignFactory.Sign sign = region.addBlockAsSign(block);
+
+			try {
+				SimpleRegionMarket.getInstance().getTemplateManager().saveRegion(region);
+			} catch (ContentSaveException e) {
+				SimpleRegionMarket.getInstance().getLogger().severe("Could not save region " + region.getName());
+				SimpleRegionMarket.getInstance().printError(e);
+			}
+
+			return sign;
+		} else {
+			player.sendMessage(ChatColor.RED + "Could not find the region.");
 		}
+		return null;
 	}
-	*/
 }
